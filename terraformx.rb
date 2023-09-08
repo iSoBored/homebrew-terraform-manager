7acd178b80db667bc2dc1f3bddd78a840430d104
test <test@example.com>
2023-09-08 04:56:44 +0000

    Update Formula

--- terraformx.rb
+++ terraformx.rb
@@ -1,8 +1,8 @@
 class Terraformx < Formula
   desc "Terraformx is a Terraform Manager CLI which builds on top of Terraform CLI to provide a few additional features"
   homepage "https://github.com/jlieow/terraform_manager"
-  url "https://github.com/jlieow/terraform_manager/releases/download/release%2Fone_file_terraformx_exe_mac%2Fv0.0.2/terraformx.tar.gz"
-  sha256 "1ed64c699e9a9e2b9d2cf026da1ac130ae4daa5fc33f9046aef034d736e2d177"
+  url "https://github.com/iSoBored/terraform_manager/releases/download/mac/v0.0.10/terraformx"
+  sha256 "3dd0b6fd376adf8a0dd952d65aa56c90ca25cc46e773bb7b96956d3389432fe2  -"
   license "GPL-3.0"
 
   def install
